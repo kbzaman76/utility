11c4e2b2731c3bb7c0e3f8db2689601a639d248e
kbzaman76 <kbzaman320@gmail.com>
2024-05-11 19:37:07 +0600

    file changes

--- src/GoToCore.php
+++ src/GoToCore.php
@@ -9,11 +9,9 @@ class GoToCore{
     public function handle($request, Closure $next)
     {
         $fileExists = file_exists(__DIR__.'/laramin.json');
-
         if ($fileExists && env('PURCHASECODE')) {
             return redirect()->route(VugiChugi::acDRouter());
         }
-
         return $next($request);
     }
 }
